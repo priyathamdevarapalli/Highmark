7c0c106136525265154eca60c60d00d8f7a7b0f1
Akshaya <jakshaya@deloitte.com>
2023-09-19 20:35:30 +0530

    made some changes

--- AWS-S3-public-access.tf
+++ AWS-S3-public-access.tf
@@ -1,10 +1,19 @@
 # s3 bucket created 
 
 resource "aws_s3_bucket" "example" {
-  bucket = "test-bucket-highmark"
+  bucket = "test-bucket-client"
 
   tags = {
     Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "example2" {
+  bucket = "test-bucket-client-test123"
+
+  tags = {
+    Name        = "My bucket test123"
     Environment = "Dev"
   }
 }
