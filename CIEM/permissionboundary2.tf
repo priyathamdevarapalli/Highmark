97ef1b94a505da6ed44f57dadea311b05a87becf
Mahithaa <mmahithaasree@deloitte.com>
2023-09-21 16:07:01 +0530

    added replication dependencies

--- CIEM/permissionboundary2.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_iam_role" "rolewithoutPB" {
-  name                = "PB_role"
-  assume_role_policy  = data.aws_iam_policy_document.instance_assume_role_policy.json # (not shown)
-  managed_policy_arns = [aws_iam_policy.policy_one.arn, aws_iam_policy.policy_two.arn]
-}
--- CIEM/permissionboundary3.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_iam_user" "userwithoutPB" {
-  name                = "PB_user"
-  assume_role_policy  = data.aws_iam_policy_document.instance_assume_role_policy.json # (not shown)
-  managed_policy_arns = [aws_iam_policy.policy_one.arn, aws_iam_policy.policy_two.arn]
-}
